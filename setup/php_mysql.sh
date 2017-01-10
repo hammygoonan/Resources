d352d1eca9a2a7d97109b13561d3aaba3c614778
Hammy Goonan <hammy@falconry.io>
2017-01-11 09:29:17 +1100

    Create php_mysql.sh

---
+++ setup/php_mysql.sh
@@ -0,0 +1,23 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+sudo apt-get install -y fail2ban git nginx mysql-server php7.1-fpm php7.1-mysql
+
+sudo adduser falconry
+sudo mkdir /home/falconry/.ssh
+sudo chmod 700 /home/falconry/.ssh
+
+sudo wget https://github.com/hammygoonan.keys -O /home/falconry/.ssh/authorized_keys
+
+sudo chmod 400 /home/falconry/.ssh/authorized_keys
+sudo chown falconry:falconry /home/falconry -R
+
+sudo mysql_secure_installation
+
+sudo usermod -aG sudo falconry
+sudo service ssh restart
+
+sudo npm install foundation-cli -g
+sudo npm install bower -g
